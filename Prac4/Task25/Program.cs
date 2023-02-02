b8172f1f6c7319e151d071eb384011fac40f4037
Eugene <lutsenko888@gmail.com>
2023-02-02 20:37:59 +0300

    task25

---
+++ Prac4/Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите желаемую степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a * a;
+int count = 3;
+while (count <= b)
+{
+    count++;
+    result = result * a;
+}
+Console.WriteLine($"{a} в степени {b} равно {result}");
