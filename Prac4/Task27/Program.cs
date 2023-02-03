f1bbc15a7359134736428b892800d6e6b207fd5e
Eugene <lutsenko888@gmail.com>
2023-02-03 06:02:09 +0300

    Task27

---
+++ Prac4/Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (n > 0)
+{
+    result = result + n % 10;
+    n = n / 10;
+}
+Console.WriteLine(result);
