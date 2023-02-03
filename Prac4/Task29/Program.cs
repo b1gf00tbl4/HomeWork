1438153bc88ea875d1fdd27ae46d62100c1033e5
Eugene <lutsenko888@gmail.com>
2023-02-03 06:30:32 +0300

    Task29

---
+++ Prac4/Task29/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Создать массив!!");
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 33);
+Console.WriteLine($"Результат: [{string.Join(" , ", array)}]");
