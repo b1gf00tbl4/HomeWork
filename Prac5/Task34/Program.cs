29cf56b122ed300d217a1e3edc7dffe9fd147e67
Eugene <lutsenko888@gmail.com>
2023-02-03 07:49:15 +0300

    Task34

---
+++ Prac5/Task34/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+void inputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int CountEvenInArray(int[] array)
+{
+    int count = 0;
+    foreach(int element in array)
+    {
+        if (element % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите количество элемнтов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+inputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {CountEvenInArray(array)}");
